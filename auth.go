fa5f5fa667a9b651fb4804a5910b54fddf173770
Danqi Ye <danqi.ye@shanda.com>
2025-04-10 17:28:03 +0800

    add auto login flow

--- auth.go
+++ auth.go
@@ -168,7 +168,7 @@ func (s *Scraper) IsLoggedIn() bool {
 
 // randomDelay introduces a random delay between 1 and 3 seconds
 func randomDelay() {
-	delay := time.Duration(3000+rand.Intn(5000)) * time.Millisecond
+	delay := time.Duration(1000+rand.Intn(2000)) * time.Millisecond
 	time.Sleep(delay)
 }
 
@@ -509,4 +509,242 @@ func (s *Scraper) sign(method string, ref *url.URL) string {
 	}
 
 	return "OAuth " + b.String()
+}
+
+func (s *Scraper) AutoLogin(credentials ...string) error {
+	var username, password, email, confirmation string
+	if len(credentials) < 2 || len(credentials) > 4 {
+		return fmt.Errorf("invalid credentials")
+	}
+
+	username, password = credentials[0], credentials[1]
+	email = credentials[2]
+	if len(credentials) == 4 {
+		confirmation = credentials[3]
+	}
+	s.setBearerToken(bearerToken2)
+
+	err := s.GetGuestToken()
+	if err != nil {
+		return err
+	}
+
+	// Initialize login flow
+	flowToken, next, err := s.initializeLoginFlow()
+	if err != nil {
+		fmt.Println("initializeLoginFlow", err)
+		return err
+	}
+	var nextData map[string]interface{}
+	var stop = false
+
+	for next != "" && !stop {
+		switch next {
+		case "LoginJsInstrumentationSubtask":
+			nextData = s.handleJsInstrumentation(flowToken)
+		case "LoginEnterUserIdentifierSSO":
+			nextData = s.handleEnterUserIdentifier(flowToken, username)
+		case "LoginEnterPassword":
+			nextData = s.handleEnterPassword(flowToken, password)
+		case "LoginTwoFactorAuthChallenge":
+			code := confirmation
+			nextData = s.handleTwoFactorAuth(flowToken, code)
+		case "AccountDuplicationCheck":
+			nextData = s.handleAccountDuplicationCheck(flowToken)
+		case "LoginEnterAlternateIdentifierSubtask":
+			nextData = s.handleLoginEnterAlternateIdentifier(flowToken, email)
+		case "LoginAcid":
+			nextData = s.handleLoginAcid(flowToken, confirmation)
+		case "LoginSuccessSubtask":
+			s.isLogged = true
+			stop = true
+			continue
+		default:
+			return fmt.Errorf("unknown subtask: %s", next)
+		}
+		fmt.Printf("current flow is %s\n", next)
+		flowToken, next, err = s.doFlow(nextData)
+
+		randomDelay() // Add a delay between requests
+	}
+	s.isOpenAccount = false
+	return err
+}
+
+func (s *Scraper) doFlow(data map[string]interface{}) (nextToken string, nextTask string, err error) {
+	info, err := s.getFlow(data)
+	if err != nil {
+		return "", "", err
+	}
+	if len(info.Errors) > 0 {
+		return "", "", fmt.Errorf("auth error (%d): %v", info.Errors[0].Code, info.Errors[0].Message)
+	}
+	next := ""
+
+	if len(info.Subtasks) > 0 {
+		if info.Subtasks[0].SubtaskID == "LoginAcid" {
+			err = fmt.Errorf("auth error: %v", "LoginAcid")
+		} else if info.Subtasks[0].SubtaskID == "DenyLoginSubtask" {
+			err = fmt.Errorf("auth error: %v", "DenyLoginSubtask")
+		} else {
+			next = info.Subtasks[0].SubtaskID
+		}
+	}
+
+	return info.FlowToken, next, err
+}
+
+func (s *Scraper) initializeLoginFlow() (string, string, error) {
+	data := map[string]interface{}{
+		"flow_name": "login",
+		"input_flow_data": map[string]interface{}{
+			"flow_context": map[string]interface{}{
+				"referrer_context": map[string]interface{}{
+					"referral_details": "utm_source=google-play&utm_medium=organic",
+					"referrer_url":     "",
+				},
+				"debug_overrides": map[string]interface{}{},
+				"start_location":  map[string]interface{}{"location": "splash_screen"},
+			},
+		},
+		"subtask_versions": map[string]int{
+			"action_list":                          2,
+			"alert_dialog":                         1,
+			"app_download_cta":                     1,
+			"check_logged_in_account":              1,
+			"choice_selection":                     3,
+			"contacts_live_sync_permission_prompt": 0,
+			"cta":                                  7,
+			"email_verification":                   2,
+			"end_flow":                             1,
+			"enter_date":                           1,
+			"enter_email":                          2,
+			"enter_password":                       5,
+			"enter_phone":                          2,
+			"enter_recaptcha":                      1,
+			"enter_text":                           5,
+			"enter_username":                       2,
+			"generic_urt":                          3,
+			"in_app_notification":                  1,
+			"interest_picker":                      3,
+			"js_instrumentation":                   1,
+			"menu_dialog":                          1,
+			"notifications_permission_prompt":      2,
+			"open_account":                         2,
+			"open_home_timeline":                   1,
+			"open_link":                            1,
+			"phone_verification":                   4,
+			"privacy_options":                      1,
+			"security_key":                         3,
+			"select_avatar":                        4,
+			"select_banner":                        2,
+			"settings_list":                        7,
+			"show_code":                            1,
+			"sign_up":                              2,
+			"sign_up_review":                       4,
+			"tweet_selection_urt":                  1,
+			"update_users":                         1,
+			"upload_media":                         1,
+			"user_recommendations_list":            4,
+			"user_recommendations_urt":             1,
+			"wait_spinner":                         3,
+			"web_modal":                            1,
+		},
+	}
+	return s.doFlow(data)
+}
+
+func (s *Scraper) handleJsInstrumentation(flowToken string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id":         "LoginJsInstrumentationSubtask",
+				"js_instrumentation": map[string]interface{}{"response": "{}", "link": "next_link"},
+			},
+		},
+	}
+}
+
+func (s *Scraper) handleEnterUserIdentifier(flowToken, username string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id": "LoginEnterUserIdentifierSSO",
+				"settings_list": map[string]interface{}{
+					"setting_responses": []map[string]interface{}{
+						{
+							"key":           "user_identifier",
+							"response_data": map[string]interface{}{"text_data": map[string]interface{}{"result": username}},
+						},
+					},
+					"link": "next_link",
+				},
+			},
+		},
+	}
+}
+
+// identifier maybe email/screen_name/phone_number.
+func (s *Scraper) handleLoginEnterAlternateIdentifier(flowToken, identifier string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id": "LoginEnterAlternateIdentifierSubtask",
+				"enter_text": map[string]interface{}{"text": identifier, "link": "next_link"},
+			},
+		},
+	}
+}
+
+func (s *Scraper) handleEnterPassword(flowToken, password string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id":     "LoginEnterPassword",
+				"enter_password": map[string]interface{}{"password": password, "link": "next_link"},
+			},
+		},
+	}
+}
+
+func (s *Scraper) handleTwoFactorAuth(flowToken, code string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id": "LoginTwoFactorAuthChallenge",
+				"enter_text": map[string]interface{}{"text": code, "link": "next_link"},
+			},
+		},
+	}
+}
+
+func (s *Scraper) handleLoginAcid(flowToken, confirmation string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id": "LoginAcid",
+				"enter_text": map[string]interface{}{"text": confirmation, "link": "next_link"},
+			},
+		},
+	}
+}
+
+func (s *Scraper) handleAccountDuplicationCheck(flowToken string) map[string]interface{} {
+	return map[string]interface{}{
+		"flow_token": flowToken,
+		"subtask_inputs": []map[string]interface{}{
+			{
+				"subtask_id": "AccountDuplicationCheck",
+				"check_logged_in_account": map[string]interface{}{
+					"link": "AccountDuplicationCheck_false",
+				},
+			},
+		},
+	}
 }
